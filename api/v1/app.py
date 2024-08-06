9c4ef5a2e29404b6153b6b150a9bdc2348fee45d
MalekAjil <aboanasajil@gmail.com>
2024-08-05 18:27:45 -0700

    task3

---
+++ api/v1/app.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""App Module"""
+from models import storage
+from api.v1.views import app_views
+
+
---
+++ api/v1/views/index.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""Index Module"""
+from api.v1.views import app_views
+
+
