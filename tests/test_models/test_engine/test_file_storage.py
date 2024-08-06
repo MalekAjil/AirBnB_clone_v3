16ad81f134b829d887cdddfd6093e5edd084bf56
MalekAjil <aboanasajil@gmail.com>
2024-08-05 18:10:54 -0700

    task3

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -113,3 +113,18 @@ class TestFileStorage(unittest.TestCase):
         with open("file.json", "r") as f:
             js = f.read()
         self.assertEqual(json.loads(string), json.loads(js))
+
+    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
+    def test_get(self):
+        """Test that save properly get objects to file.json"""
+        storage = FileStotage()
+        test_dict = {}
+        print(storage.get())
+
+    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
+    def test_count(self):
+        """Test that save properly get objects to file.json"""
+        storage = FileStotage()
+        print(storage.count())
+
+
